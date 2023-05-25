281b8f15ece08fa86a1aa108773a104e61f7eac5
neoplasmes <zverkovegor@gmail.com>
2023-05-25 16:02:01 +0300

    Initial Commit for this project

--- src/main/java/com/example/auidoplayer/playTrackButton.java
+++ src/main/java/com/example/auidoplayer/playTrackButton.java
@@ -1,12 +1,35 @@
 package com.example.auidoplayer;
 
-import javafx.scene.control.Button;
+import javafx.event.EventHandler;
+import javafx.scene.control.Label;
+import javafx.scene.input.MouseButton;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
 
-public class playTrackButton extends Button {
+import java.io.File;
 
-    public playTrackButton(){
+public class playTrackButton extends Label {
+
+    protected String path;
+    protected String name;
+    protected String artist;
+
+    public playTrackButton(File file, String id){
+        path = file.getAbsolutePath();
+
+        name = file.getName();
+
+
+        //стиль
+        super.setText(path + "\n" + name);
+        super.setId(id);
+        super.setMinHeight(50);
+        super.setStyle("-fx-border-color: #000000");
 
     }
+
+
 
 
 
