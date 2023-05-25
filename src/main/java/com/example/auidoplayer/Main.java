b9a147548fdb576c67e04339a34bcb74e7cb8e17
neoplasmes <zverkovegor@gmail.com>
2023-05-25 13:30:16 +0300

    Initial Commit for this project

---
+++ src/main/java/com/example/auidoplayer/Main.java
@@ -0,0 +1,45 @@
+package com.example.auidoplayer;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+import java.io.IOException;
+
+import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.event.EventHandler;
+import javafx.fxml.FXMLLoader;
+import javafx.stage.Stage;
+import javafx.stage.WindowEvent;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage stage) throws IOException {
+
+        Parent root = FXMLLoader.load(getClass().getResource("css.fxml"));
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+
+        stage.setOnCloseRequest(new EventHandler<WindowEvent>() {
+
+            @Override
+            public void handle(WindowEvent arg0) {
+
+                Platform.exit();
+                System.exit(0);
+            }
+        });
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
